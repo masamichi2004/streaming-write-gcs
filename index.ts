7cd5fb15283f3decf352f028c06f09ecad27468e
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2024-08-12 10:39:20 +0900

    feat: add gcs to gcs stream logic

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 import { Storage } from "@google-cloud/storage";
 import { config } from "dotenv";
-import * as fs from 'fs';
 
 config();
 
-const storage = new Storage();
+const storage = new Storage({projectId: process.env.PROJECT_ID});
 const BUCKET_NAME = 'gcs_study_bucket';
-const SOURCE_CSV_NAME = 'gcs_study_mock.csv';
-const DEST_CSV_NAME = 'gcs_study_mock_data_stream.csv';
+const SOURCE_CSV_NAME = 'gcs_study_mock_data.csv';
+const DEST_CSV_NAME = 'gcs_study_mock_data_stream_copy.csv';
 
 
 async function uploadCSVToGCSStream(){
     const bucket = storage.bucket(BUCKET_NAME);
+
+    // GCSのCSVファイルをストリームで読み込む
+    const readStream = bucket
+    .file(SOURCE_CSV_NAME)
+    .createReadStream();
 
     const uploadStream = bucket
     .file(DEST_CSV_NAME)
@@ -20,7 +24,7 @@ async function uploadCSVToGCSStream(){
     });
 
     // SOURCE_CSV_NAMEをストリーミングで読み込んでGCSにストリームWriteするパイプ操作を実行
-    fs.createReadStream(SOURCE_CSV_NAME).pipe(uploadStream);
+    readStream.pipe(uploadStream);
 
     uploadStream.on('finish', () => {
         console.log(`${DEST_CSV_NAME} uploaded to ${BUCKET_NAME}`);
